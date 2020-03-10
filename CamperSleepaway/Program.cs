6cd64931d35085ac8978fd1594b69fe9c9b6e3d8
Daniel Andersson <danzigdanne@gmail.com>
2020-03-10 16:06:28 +0100

    added menu;

--- CamperSleepaway/Program.cs
+++ CamperSleepaway/Program.cs
@@ -6,6 +6,13 @@ namespace CamperSleepaway
     {
         static void Main(string[] args)
         {
+            while (true){
+                Console.WriteLine("Välj 1. Daniel \n 2. Jonathan \n 3. Linus");
+                string respons = Console.ReadLine();
+                if (respons == 1) JonathanTest();
+                if (respons == 2) LinusTest();
+                if (respons == 3) DanielTest();
+            }
             
         }
     }
