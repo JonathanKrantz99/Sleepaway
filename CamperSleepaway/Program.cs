30f127f37a53024f90fdb4f83a0704bf313ea682
Daniel Andersson <danzigdanne@gmail.com>
2020-03-10 16:14:57 +0100

    en till menu

--- CamperSleepaway/Program.cs
+++ CamperSleepaway/Program.cs
@@ -7,8 +7,8 @@ namespace CamperSleepaway
         static void Main(string[] args)
         {
             while (true){
-                Console.WriteLine("Välj 1. Daniel \n 2. Jonathan \n 3. Linus");
-                string respons = Console.ReadLine();
+                Console.WriteLine("Välj\n 1. Daniel \n 2. Jonathan \n 3. Linus");
+                int respons = Convert.ToInt32(Console.ReadLine());
                 if (respons == 1) Jonathan.Program.JonathanTest();
                 if (respons == 2) Linus.Program.LinusTest();
                 if (respons == 3) Daniel.Program.DanielTest();
